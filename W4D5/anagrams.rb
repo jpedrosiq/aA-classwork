9954bee67c782c09cd5b737414a709786ac70d1e
jpedrosiq <johnpedrosiqueira@gmail.com>
2020-03-07 13:40:06 -0800

    adding big(O)

---
+++ W4D5/anagrams.rb
@@ -0,0 +1,73 @@
+
+# PHASE 1
+
+def first_anagrams?(str1, str2) # O(n!)
+  result = str1.chars.permutation(str1.length).to_a # O(n!)
+
+  result.map! do |word_arr| # O(n^2)
+    word_arr.join("")
+  end
+
+  result.include?(str2)     # O(n)
+end
+
+# p first_anagrams?("gizmo", "sally")    #=> false
+# p first_anagrams?("elvis", "lives")    #=> true
+
+# PHASE 2
+
+def second_anagram?(str1, str2)   # O(nm)
+  str2_arr = str2.chars
+  str1.each_char do |char|     
+    if str2_arr.include?(char)    
+      index_deleted = str2_arr.find_index(char) 
+      str2_arr.delete_at(index_deleted) 
+    end
+  end
+  str2_arr.length == 0
+end
+
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+#PHASE 3
+def third_anagram?(str1, str2) #O(n log n)
+  str1.chars.sort == str2.chars.sort
+end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+
+# PHASE 4
+def fourth_anagram?(str1, str2) # O(n)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+  str1.each_char do |char|
+    hash1[char] += 1
+  end
+  str2.each_char do |char|
+    hash2[char] += 1
+  end
+  hash1 == hash2
+
+end
+
+
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")    #=> true
+
+#BONUS
+
+def bonus_anagram?(str1, str2) # O(n)
+  hash1 = Hash.new(0)
+  str1.each_char do |char|
+    hash1[char] += 1
+  end
+  str2.each_char do |char|
+    hash1[char] -= 1
+  end
+  hash1.values.all? {|value| value == 0}
+end
+p bonus_anagram?("gizmo", "sally")    #=> false
+
+p bonus_anagram?("elvis", "lives")    #=> true
---
+++ W4D5/exectime.rb
@@ -0,0 +1,71 @@
+
+# PHASE I
+
+def my_min(list)        # time complexity = O(n)
+  min_num = list.first
+  (1...list.length).each do |i|
+    if min_num > list[i]
+      min_num = list[i]
+    end
+  end
+  min_num
+end
+
+# p my_min([0,3,5,4,-5,10,1,90])
+
+# PHASE 2
+
+def my_min(list)   # time complexity = O(n)
+  min = list.min
+end
+# p my_min([0,3,5,4,-5,10,1,90])
+
+#PHASE I
+def largest_contiguous_subsum(list) # O(n^3)
+  result = []
+  i = 0
+  while i < list.length
+    j = i + 1
+    result << [list[i]]
+    while j < list.length 
+      result << list[i..j]
+      j += 1
+    end
+    i += 1
+  end
+  max = 0
+  result.each do |subarr|
+    if subarr.sum > max
+      max = subarr.sum
+    end
+  end
+  max
+end
+
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list) # => 8
+
+#Phase II
+def largest_contiguous_subsum(list)   # O(2n) = O(n)
+  current = 0      
+  largest = list.first
+  i = 0
+  # return list.max if list.all? { |ele| ele < 0 }
+
+  while i < list.length       # i = 5
+    current += list[i]        # current = 7 - 6 = 1 + 7
+    if largest < current      # 7 < 8
+      largest = current       # largest = 8
+    end
+    if current < 0      # false
+      current = 0       # current = 7
+    end
+    i+=1            
+  end
+  
+  largest            # largest = 8
+end 
+
+p largest_contiguous_subsum([2, 3, -6, 7, -6, 7]) # => 8
+p largest_contiguous_subsum([5, 3, -7]) # => 8
+p largest_contiguous_subsum([-5,-1,-3]) # => -1
